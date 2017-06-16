4076513b3dba8e77a01472ca2cbd07b8ca49709c
manaten <manaten@manaten.net>
2017-06-16 18:06:57 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,45 @@
+/*
+*
+*  Push Notifications codelab
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
+
+/* eslint-env browser, serviceworker, es6 */
+
+'use strict';
+
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  console.log(data);
+  console.log('[Service Worker] Push Received.');
+  console.log('[Service Worker] Push had this data:', data);
+
+  const title = data.title;
+  const options = {
+    body: data.body,
+    icon: data.icon,
+    badge: data.badge,
+    data: data
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+  event.notification.close();
+  console.log(event.notification.data);
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
